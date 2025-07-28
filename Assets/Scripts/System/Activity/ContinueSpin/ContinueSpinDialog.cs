5b193ab1c7f6c8cdf5f638e887eee14f8fd3bdb2
yasin <yasinc@qq.com>
2025-07-28 19:28:14 +0800

    Revert "关闭弹窗去掉广告"
    This reverts commit 1d526de85ee4d18616d72e616645b8b92edb1b63.

--- Assets/Scripts/System/Activity/ContinueSpin/ContinueSpinDialog.cs
+++ Assets/Scripts/System/Activity/ContinueSpin/ContinueSpinDialog.cs
@@ -137,7 +137,7 @@ namespace Activity
         }
         private void OnClickCloseBtn()
         {
-            bool rewardADIsReady = false;//ADManager.Instance.InterstitialAdIsOk(ADEntrances.REWARD_VIDEO_CONTINUE_SPIN);
+            bool rewardADIsReady = ADManager.Instance.InterstitialAdIsOk(ADEntrances.REWARD_VIDEO_CONTINUE_SPIN);
               
             if (rewardADIsReady)
             {

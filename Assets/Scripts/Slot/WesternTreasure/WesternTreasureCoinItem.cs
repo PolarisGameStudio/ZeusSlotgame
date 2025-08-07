7017d55ffc860a00c923532ab5e39af76a17fc24
yasin <yasinc@qq.com>
2025-08-07 17:33:24 +0800

    解决图片消失的问题

--- Assets/Scripts/Slot/WesternTreasure/WesternTreasureCoinItem.cs
+++ Assets/Scripts/Slot/WesternTreasure/WesternTreasureCoinItem.cs
@@ -75,7 +75,7 @@ public class WesternTreasureCoinItem : MonoBehaviour
                 animator.Play("JackpotGame_show");
                 break;
             case 3:
-                animator.Play("JackpotGame_win");
+                //animator.Play("JackpotGame_win");
                 
                 break;
             default:

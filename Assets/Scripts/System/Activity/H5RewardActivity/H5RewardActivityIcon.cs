0579b259c7a254b88adccbb724cfdac516035d7e
Huohua1518 <1923363627@qq.com>
2025-08-08 14:45:03 +0800

    同步h5 按钮

--- Assets/Scripts/System/Activity/H5RewardActivity/H5RewardActivityIcon.cs
+++ Assets/Scripts/System/Activity/H5RewardActivity/H5RewardActivityIcon.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Libs;
 using Spine.Unity;
 using Spine.Unity.Modules;
 using UnityEngine;
@@ -60,6 +61,8 @@ namespace Activity
                 ChangeSkinByIndex(curShowIndex);
                 clickButton.enabled = true;
                 skeletonGraphic.gameObject.SetActive(true);
+                //检查是否需要显示气泡，默认不显示
+                UpdateQiPao(PlatformManager.Instance.GetH5UserType() == 1);
                 // saqian.gameObject.SetActive(true);
                 // NeedShowQiPao();
             }
@@ -71,7 +74,15 @@ namespace Activity
                 // DestroyShowQiPao();
             }
         }
-        
+
+        private void UpdateQiPao(bool isShow)
+        {
+            if (!isShow)
+            {
+                skeletonGraphic.AnimationState.SetAnimation(0,"without_pao",true);
+            }
+        }
+
         public void ChangeSkinByIndex(int index)
         {
             if (skeletonGraphic != null)

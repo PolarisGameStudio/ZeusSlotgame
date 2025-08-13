e401284e5cb93c8a25b7db4cb5e0b5c45c394020
Huohua1518 <1923363627@qq.com>
2025-08-13 10:06:33 +0800

    修复收集 wild 图标任务次数不正确弹出提示进度的 bug

--- Assets/Scripts/System/WithDraw/WithDrawManager.cs
+++ Assets/Scripts/System/WithDraw/WithDrawManager.cs
@@ -83,7 +83,7 @@ namespace System
                 return;
             }
             Debug.Log($"[WithDrawManager][OnSpinAwardEnd] FreeSymbolNum:{FreeSymbolNum}");
-            int AddNumber = reelManager.GetSpecialCount(SymbolMap.IS_FREESPIN);
+            int AddNumber = reelManager.GetSpecialCount(SymbolMap.IS_WILD);
             long remin = FreeSymbolNum % 10;
             FreeSymbolNum += AddNumber;
             if (remin+AddNumber<10)

d643c646d7aa5562ed84d70fd751b6fea2f92abb
Huohua1518 <1923363627@qq.com>
2025-08-13 20:11:09 +0800

    修改曲线

--- Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/CommonUI/CoinsBezier.cs
+++ Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/CommonUI/CoinsBezier.cs
@@ -555,8 +555,8 @@ namespace Libs
 
 			// 路径设定
 			bezierMiddlePoints.Add(new List<Vector3>());
-			bezierMiddlePoints[0].Add(new Vector3(-1.5f, 0.3177f, 0f));
-			bezierMiddlePoints[0].Add(new Vector3(1.1f, 0.5110f, 0f));
+			bezierMiddlePoints[0].Add(new Vector3(-5f, 0.4f, 0f));
+			bezierMiddlePoints[0].Add(new Vector3(5f, 0.7f, 0f));
 
 			bezierMiddlePoints.Add(new List<Vector3>());
 			bezierMiddlePoints[1].Add(new Vector3(-1.2f, 0.299487f, 0f));

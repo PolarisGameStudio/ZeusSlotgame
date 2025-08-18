499347fbd46d9ccd54f327ffae5511954953fe39
Huohua1518 <1923363627@qq.com>
2025-08-18 14:09:35 +0800

    300模式添加tasktip显示

--- Assets/Scripts/Lobby/UI/TaskTipPanel.cs
+++ Assets/Scripts/Lobby/UI/TaskTipPanel.cs
@@ -32,22 +32,39 @@ public class TaskTipPanel : MonoBehaviour
         { TaskConstants.WatchADTimeTask_Key, 1 },
         { TaskConstants.CollectCardTask_Key, 1 }
     };
-    // Start is called before the first frame update
-    void Awake()
-    {
-        // txtInfo = transform.Find("txt_info").GetComponent<UIText>();
-        // progress = transform.Find("slider_progress/txt_progress").GetComponent<UIText>();
-        // slider = transform.Find("slider_progress/img_bar").GetComponent<Image>();
-        // slider.fillAmount = 0f;
-        // progress.SetText("");
-    }
 
     public void RefreshInfo(int taskType)
     {
+        txtInfo = transform.Find("txt_info").GetComponent<UIText>();
+        progress = transform.Find("slider_progress/txt_progress").GetComponent<UIText>();
+        slider = transform.Find("slider_progress/img_bar").GetComponent<Image>();
+        particlePar= transform.Find("slider_progress/img_bar/UIParticle");
+        particle1= transform.Find("slider_progress/img_bar/UIParticle/lizi").GetComponent<ParticleSystem>();
+        particle2= transform.Find("slider_progress/img_bar/UIParticle/lizi1").GetComponent<ParticleSystem>();
+        image_head = transform.Find("slider_progress/img_bar/img_head").GetComponent<Image>();
+        progressBar = slider.GetComponent<RectTransform>();
         gameObject.SetActive(false);
+        //300模式下，只显示金钱的进度
+        
         BaseTask task = TaskTipManager.Instance.GetTask(taskType);
         if (task==null)
         {
+            // if (TaskTipManager.Instance.CheckShow300CashTip())
+            // {
+            //     //显示现金的钱
+            //     image_icon.sprite = GetSpriteByType(TaskConstants.CollectCashFromZeroTask_Key);
+            //     txtInfo.gameObject.SetActive(false);
+            //     txtInfo = transform.Find("txt_info1").GetComponent<UIText>();
+            //     txtInfo.gameObject.SetActive(true);
+            //     txtInfo.SetText(TaskTipManager.Instance.Get300CashStr());
+            //     int maxValue = OnLineEarningMgr.Instance.GetMaxValue()* OnLineEarningMgr.Instance.GetCashMultiple();
+            //     float f1 = OnLineEarningMgr.Instance.Cash()/(maxValue*1.0f);
+            //     string info = string.Format("{0}/{1}",OnLineEarningMgr.Instance.GetMoneyStr((int)OnLineEarningMgr.Instance.Cash(),2,false,true),
+            //         OnLineEarningMgr.Instance.GetMoneyStr((int)maxValue,2,false,true));
+            //     HandleShowProgressBar(f1,info);
+            //     DelayShowSelf();
+            //     return;
+            // }
             Debug.LogError("TaskTipPanel task is null, taskType: " + taskType);
             return;
         }
@@ -61,11 +78,6 @@ public class TaskTipPanel : MonoBehaviour
         background.sprite = backgroundSprites[TaskToBGSpriteIndex.TryGetValue(task.TaskType, out int index) ? index : 0];
         //设置图标
         image_icon.sprite = GetSpriteByType(task.TaskType);
-        
-        txtInfo = transform.Find("txt_info").GetComponent<UIText>();
-        progress = transform.Find("slider_progress/txt_progress").GetComponent<UIText>();
-        slider = transform.Find("slider_progress/img_bar").GetComponent<Image>();
-        progressBar = slider.GetComponent<RectTransform>();
         slider.fillAmount = 0f;
         slider.sprite = sliderSprites[index];
         if (index>0)
@@ -75,35 +87,10 @@ public class TaskTipPanel : MonoBehaviour
             txtInfo.gameObject.SetActive(true);
         }
         progress.SetText("");
-        particlePar= transform.Find("slider_progress/img_bar/UIParticle");
-        particle1= transform.Find("slider_progress/img_bar/UIParticle/lizi").GetComponent<ParticleSystem>();
-        particle2= transform.Find("slider_progress/img_bar/UIParticle/lizi1").GetComponent<ParticleSystem>();
-        image_head = transform.Find("slider_progress/img_bar/img_head").GetComponent<Image>();
-
         //获取提示文本
         txtInfo.SetText(TaskTipManager.Instance.GetTaskTipText(task));
         float f = task.HasCollectNum/(task.TargetNum * 1f);
-        if (particlePar!=null)
-        {
-            Vector3 endPosition = new Vector3(progressBar.rect.width * f - progressBar.rect.width/2, 0, 0);
-            image_head.transform.localPosition = endPosition;
-            // 转换坐标并更新
-            Vector3 worldPos = progressBar.TransformPoint(endPosition);
-            particlePar.position = worldPos;
-            float particleLifeTime = Mathf.Max(maxLifeTime * f,0.05f);
-            if (particle1 != null)
-            {
-                ParticleSystem.MainModule main = particle1.main;
-                main.startLifetime = particleLifeTime;
-            }
-            if (particle2 != null)
-            {
-                ParticleSystem.MainModule main = particle2.main;
-                main.startLifetime = particleLifeTime;
-            }
-        }
-        slider.fillAmount = f;
-        progress.SetText(task.GetProgressDesc());
+        HandleShowProgressBar(f, task.GetProgressDesc());
         DelayShowSelf();
     }
     
@@ -149,6 +136,32 @@ public class TaskTipPanel : MonoBehaviour
 
         return sp;
     }
+
+
+    void HandleShowProgressBar(float f,string info)
+    {
+        if (particlePar!=null)
+        {
+            Vector3 endPosition = new Vector3(progressBar.rect.width * f - progressBar.rect.width/2, 0, 0);
+            image_head.transform.localPosition = endPosition;
+            // 转换坐标并更新
+            Vector3 worldPos = progressBar.TransformPoint(endPosition);
+            particlePar.position = worldPos;
+            float particleLifeTime = Mathf.Max(maxLifeTime * f,0.05f);
+            if (particle1 != null)
+            {
+                ParticleSystem.MainModule main = particle1.main;
+                main.startLifetime = particleLifeTime;
+            }
+            if (particle2 != null)
+            {
+                ParticleSystem.MainModule main = particle2.main;
+                main.startLifetime = particleLifeTime;
+            }
+        }
+        slider.fillAmount = f;
+        progress.SetText(info);
+    }
     
     private void DelayShowSelf()
     {
@@ -158,5 +171,11 @@ public class TaskTipPanel : MonoBehaviour
             gameObject.SetActive(true);
             transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBack).SetUpdate(true);
         }).Play();
+    }
+
+    private void HandleOnLine300Show()
+    {
+        float f1 = OnLineEarningMgr.Instance.Cash()/(OnLineEarningMgr.Instance.GetMaxValue() * 1.0f);
+        
     }
 }

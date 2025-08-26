b1e7445b4918180765a3af4ca2fd8050d6766f6c
Huohua1518 <1923363627@qq.com>
2025-08-26 16:39:30 +0800

    app退出时缓存配置修改

--- Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/GameConsole/BaseGameConsole.cs
+++ Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/GameConsole/BaseGameConsole.cs
@@ -424,11 +424,7 @@ public class BaseGameConsole :MonoBehaviour
 		TaskManager.Instance.SaveProgressData();
 		UserManager.GetInstance().UserProfile().IsFirstGameSession = false;
 		UserManager.GetInstance ().UserProfile ().SaveToPlayerPrefs ();
-		CardSystemManager.Instance.SaveProgressData();
-		WithDrawManager.Instance.SaveProgressData();
 		ADManager.Instance.SaveADProgressData();
-		TaskTipManager.Instance.SaveProgressData();
-
 	}
 	
 	void Update()

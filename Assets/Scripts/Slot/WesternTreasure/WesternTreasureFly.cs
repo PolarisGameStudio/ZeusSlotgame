3b98ed729a3051e6d0cd7429562c370665a749e9
Huohua1518 <1923363627@qq.com>
2025-09-10 11:40:37 +0800

    修改播放jackpotstart音效名称

--- Assets/Scripts/Slot/WesternTreasure/WesternTreasureFly.cs
+++ Assets/Scripts/Slot/WesternTreasure/WesternTreasureFly.cs
@@ -182,7 +182,7 @@ public class WesternTreasureFly : MonoBehaviour
             AudioManager.Instance.AsyncPlayEffectAudio("treelevelup");
             yield return new WaitForSeconds(1f);
             Messenger.Broadcast<string>(GameConstants.PlayFullScreenAnimation, "JackPotStart");
-            AudioManager.Instance.AsyncPlayEffectAudio("JackPotStart");
+            AudioManager.Instance.AsyncPlayEffectAudio("JackPotStart1");
             yield return new WaitForSeconds(5.8f);
             // SetTriggerEffect(false);
             treeManager.OpenJackpotGame();

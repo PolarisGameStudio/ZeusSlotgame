1fdfaa7918f2e618f827f2c003e0e0980b999cd2
yasin <yasinc@qq.com>
2025-09-24 19:58:21 +0800

    兑换商店功能提交
    # Conflicts:
    #	Assets/AssetResources

---
+++ Assets/Scripts/System/Activity/WheelLuckActivity/WheelLuckGetRewardDialog.cs
@@ -0,0 +1,68 @@
+﻿using Ads;
+using Libs;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Activity
+{
+    public class WheelLuckGetRewardDialog:UIDialog   
+    {
+        private int _shopItemId;
+        private Button _btnCollect;
+        private Button _btnClaim;
+        private Image _imageCard;
+        private WheelLuckActivity _activity;
+        protected override void Awake()
+        {
+            base.Awake();
+            _btnCollect = Util.FindObject<Button>(transform, "Anchor/btn_collect");
+            _btnClaim = Util.FindObject<Button>(transform, "Anchor/btn_claim");
+            _imageCard = Util.FindObject<Image>(transform, "Anchor/item_card/Icon");
+            _btnCollect.onClick.AddListener(OnClickMutCountBtn);
+            _btnClaim.onClick.AddListener(OnClickClaimBtn);
+            _activity = ActivityManager.Instance.GetActivityByID(WheelLuckActivity.ActiveId) as WheelLuckActivity;
+        }
+        private void OnClickClaimBtn()
+        {
+            Messenger.Broadcast(GameDialogManager.OpenWheelLuckReceiveCardDialogMsg,_shopItemId,false);
+            Close();
+        }
+        private void OnClickMutCountBtn()
+        {
+            WheelLuckActivity.AdType = WheelLuckAdType.MultipleReward;
+            Messenger.Broadcast<string>(ADConstants.PlayAdByEntrance,ADEntrances.REWARD_VIDEO_WHEELLUCK_SPIN);
+        }
+        
+        protected override void OnDisable()
+        {
+            Messenger.RemoveListener<int>(ADConstants.PlayWheelLuckAD, ShowVideoCallBack);
+            Messenger.RemoveListener<int>(ADConstants.PlayWheelLuckADFailed, ShowVideoCallBack);
+        }
+        
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Messenger.AddListener<int>(ADConstants.PlayWheelLuckAD, ShowVideoCallBack);
+            Messenger.AddListener<int>(ADConstants.PlayWheelLuckADFailed, ShowVideoCallBack);
+        }
+        
+        private void ShowVideoCallBack(int arg0)
+        {
+            if( WheelLuckActivity.AdType != WheelLuckAdType.MultipleReward) return;
+            
+            Messenger.Broadcast(GameDialogManager.OpenWheelLuckReceiveCardDialogMsg,_shopItemId,true);
+            
+            Close();
+        }
+        
+        public void SetUIData(int shopItemId)
+        {
+            _shopItemId = shopItemId;
+            ShowCardItem();
+        }
+
+        private void ShowCardItem()
+        {
+            _activity.LoadIcon(_shopItemId,_imageCard);
+        }
+    }
+}

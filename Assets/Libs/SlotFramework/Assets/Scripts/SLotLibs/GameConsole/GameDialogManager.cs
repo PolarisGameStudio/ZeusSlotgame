5b230a3b49eed27f4601e258c9579d7d1a3be9f1
yasin <yasinc@qq.com>
2025-09-24 20:02:35 +0800

    解决代码冲突

--- Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/GameConsole/GameDialogManager.cs
+++ Assets/Libs/SlotFramework/Assets/Scripts/SLotLibs/GameConsole/GameDialogManager.cs
@@ -645,7 +645,7 @@ public class GameDialogManager : MonoBehaviour
 		
 	}
 	
-	protected virtual void OpenContinueSpinDialog(int activityId,Action callback)
+	protected virtual void OpenContinueSpinDialog(int activityId)
 	{
 		
 	}

540341d8cbba9d1ae18f223a480bbaf02d205772
yasin <yasinc@qq.com>
2025-10-11 14:58:36 +0800

    添加获取礼物打点

--- Assets/Scripts/System/CardSystem/UI/CardSystemGetGiftDialog.cs
+++ Assets/Scripts/System/CardSystem/UI/CardSystemGetGiftDialog.cs
@@ -7,24 +7,23 @@ namespace CardSystem
     public class CardSystemGetGiftDialog:UIDialog   
     {
         private Button _btnCollect;
-        //private Image _imgIcon;
         private Text _tips;
-        //private TextMeshProUGUI _name;
-        //private Transform _cardImage;
+
+        private const string GetCardGiftCount = "GetCardGiftCount";
         protected override void Awake()
         {
             base.Awake();
             _tips = Util.FindObject<Text>(transform, "Anchor/Tips");
-            //_name = Util.FindObject<TextMeshProUGUI>(transform, "Anchor/Name");
             _btnCollect = Util.FindObject<Button>(transform, "Anchor/btn_collect");
-            //_imgIcon = Util.FindObject<Image>(transform, "Anchor/content/Icon");
-            //_cardImage = Util.FindObject<Transform>(transform, "Anchor/content/Card");
             _btnCollect.onClick.AddListener(BtnCloseClick);
         }
         
         public void SetUIData()
         {
-            //_cardImage.gameObject.SetActive(false);
+            var allCount = SharedPlayerPrefs.GetPlayerPrefsIntValue(GetCardGiftCount);
+            allCount++;
+            PlatformManager.Instance.SendMsgToPlatFormByType(MessageType.BuryPoint, GetCardGiftCount, allCount);
+            SharedPlayerPrefs.SetPlayerPrefsIntValue(GetCardGiftCount,allCount);
 
             var amount = CardSystemManager.Instance.GetRandomMoneyReward();
           

97d4c9dc9568753c6e29be2c248fa628bf2bac06
Huohua1518 <1923363627@qq.com>
2026-01-15 20:58:02 +0800

    上报每天的子任务完成bug修复

--- Assets/Scripts/System/WithDraw/RedeemItemData.cs
+++ Assets/Scripts/System/WithDraw/RedeemItemData.cs
@@ -207,11 +207,11 @@ namespace System
 
         private void OnSequentialTaskChildTaskCompleted(BaseTask childTask)
         {
+            //上报每天的子任务完成
+            WithDrawManager.Instance.SendTaskMsg(childTask.TaskId);
             if (childTask.IsSpinRelated())
             {
                 haveTaskCompleted = true;
-                //上报每天的子任务完成
-                WithDrawManager.Instance.SendTaskMsg(childTask.TaskId);
                 ShowTaskCompleteDialogAsync(childTask).Forget();
             }
             else
